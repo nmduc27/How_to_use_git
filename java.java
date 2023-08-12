9a088f8868f7c30304d7390a9e104059778f31c4
nmduc27 <141427309+nmduc27@users.noreply.github.com>
2023-08-13 05:13:53 +0700

    Update java.java

--- java.java
+++ java.java
@@ -3,5 +3,7 @@ public class java
     public static void main(String []args)
     {
         System.out.println("My First Java Program.");
+        System.out.println("My Second Java Program.");
+
     }
 };
